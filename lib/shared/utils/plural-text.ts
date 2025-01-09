a702f196e675b74e4fe71a0db2538aa9e641cf23
johnshift <johnshift.dev@gmail.com>
2025-01-09 20:21:31 +0800

    refactor: add plural-text util fn

---
+++ lib/shared/utils/plural-text.ts
@@ -0,0 +1,9 @@
+export const pluralText = (text: string | [string, string], count: number) => {
+  const isPlural = count > 1;
+
+  if (Array.isArray(text)) {
+    return isPlural ? text[0] : text[1];
+  }
+
+  return `${text}${isPlural ? 's' : ''}`;
+};
