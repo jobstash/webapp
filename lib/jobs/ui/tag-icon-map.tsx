585b9d117f5890375e75777349410b985588408e
johnshift <johnshift.dev@gmail.com>
2025-01-09 21:09:32 +0800

    refactor: init info tag mappings

---
+++ lib/jobs/ui/tag-icon-map.tsx
@@ -0,0 +1,28 @@
+export const jobInfoTagsMap: Record<string, React.ReactNode> = {
+  seniority: null,
+  salary: null,
+  location: null,
+  workMode: null,
+  commitment: null,
+  paysInCrypto: null,
+  offersTokenAllocation: null,
+  classification: null,
+};
+
+export const jobProjectInfoTagsMap: Record<string, React.ReactNode> = {
+  tokenSymbol: null,
+  category: null,
+};
+
+export const jobProjectTvlTagsMap: Record<string, React.ReactNode> = {
+  tvl: null,
+  monthlyVolume: null,
+  monthlyActiveUsers: null,
+  monthlyFees: null,
+  monthlyRevenue: null,
+};
+
+export const jobProjectAuditTagsMap: Record<string, React.ReactNode> = {
+  audit: null,
+  hack: null,
+};
