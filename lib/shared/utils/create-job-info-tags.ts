5c89958c4bf04cbdf613ce8e62aaea6c48108695
johnshift <johnshift.dev@gmail.com>
2025-01-09 21:06:52 +0800

    refactor: add create-job-info-tags util fn

---
+++ lib/shared/utils/create-job-info-tags.ts
@@ -0,0 +1,93 @@
+import 'server-only';
+
+import { JobListItemDto } from '@/lib/jobs/api/dtos';
+import { jobSeniorityMapping } from '@/lib/jobs/core/constants';
+import { MappedInfoTagSchema } from '../core/schemas';
+import { formatNumber } from './format-number';
+import { titleCase } from './title-case';
+import { capitalize } from './capitalize';
+
+export const createJobInfoTags = (dto: JobListItemDto) => {
+  const {
+    seniority,
+    locationType,
+    location,
+    commitment,
+    paysInCrypto,
+    offersTokenAllocation,
+    classification,
+  } = dto;
+
+  const tags: MappedInfoTagSchema[] = [];
+
+  if (seniority && seniority in jobSeniorityMapping) {
+    tags.push({
+      iconKey: 'seniority',
+      label: jobSeniorityMapping[seniority as keyof typeof jobSeniorityMapping],
+    });
+  }
+
+  const salaryText = getSalaryText(dto);
+  if (salaryText) {
+    tags.push({
+      iconKey: 'salary',
+      label: `Salary: ${salaryText}`,
+    });
+  }
+
+  if (locationType) {
+    tags.push({
+      iconKey: 'workMode',
+      label: capitalize(locationType, true),
+    });
+  }
+
+  if (location) {
+    tags.push({
+      iconKey: 'location',
+      label: capitalize(location),
+    });
+  }
+
+  if (commitment) {
+    tags.push({
+      iconKey: 'commitment',
+      label: titleCase(commitment),
+    });
+  }
+
+  if (paysInCrypto) {
+    tags.push({
+      iconKey: 'paysInCrypto',
+      label: 'Pays in Crypto',
+    });
+  }
+
+  if (offersTokenAllocation) {
+    tags.push({
+      iconKey: 'offersTokenAllocation',
+      label: 'Offers Token Allocation',
+    });
+  }
+
+  if (classification) {
+    tags.push({
+      iconKey: 'classification',
+      label: titleCase(classification),
+    });
+  }
+
+  return tags;
+};
+
+const getSalaryText = (dto: JobListItemDto) => {
+  const { minimumSalary, maximumSalary, salary, salaryCurrency } = dto;
+
+  if (minimumSalary && maximumSalary) {
+    return `${formatNumber(minimumSalary)} - ${formatNumber(maximumSalary)}`;
+  }
+
+  if (salary && salaryCurrency) return `${formatNumber(salary)} ${salaryCurrency}`;
+
+  return null;
+};
