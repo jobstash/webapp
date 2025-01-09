229e1a1fed7bea7d3d2677d85ef1b8fd82765816
johnshift <johnshift.dev@gmail.com>
2025-01-09 19:14:22 +0800

    refactor: add default timeout for ky fetch

--- lib/shared/data/ky-fetch.ts
+++ lib/shared/data/ky-fetch.ts
@@ -1,8 +1,11 @@
 import ky from 'ky';
 
+const DEFAULT_TIMEOUT = 15_000;
+
 export const kyFetch = ky.create({
   credentials: 'include',
   mode: 'cors',
+  timeout: DEFAULT_TIMEOUT,
   // headers: {
   //   'ETag': '...',
   //   'If-None-Match': '...'
