cf3df89e93ee8c3bd75311cb8162b046179a179f
johnshift <johnshift.dev@gmail.com>
2025-01-10 23:33:22 +0800

    refactor: init job-list infinite list

---
+++ lib/jobs/ui/job-list.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import { useInView } from 'react-intersection-observer';
+
+import useSWRInfinite from 'swr/infinite';
+
+import { fetchJobListPage } from '@/lib/jobs/api/fetch-job-list-page';
+
+import { JobListItem } from '@/lib/jobs/ui/job-list-item';
+
+export const JobList = () => {
+  const { data, isLoading, size, setSize } = useSWRInfinite(
+    (page: number) => ({ page: page + 2 }),
+    ({ page }) => fetchJobListPage({ page }),
+  );
+
+  const { ref } = useInView({
+    threshold: 1,
+    onChange: (inView) => {
+      if (inView && !isLoading) {
+        setSize(size + 1);
+      }
+    },
+  });
+
+  if (!data) return <p>LOADING ...</p>;
+
+  return (
+    <>
+      {data
+        .flatMap((d) => d.data)
+        .map((job) => (
+          <JobListItem key={job.id} job={job} />
+        ))}
+      <div className='py-20'>
+        <p ref={ref}>Loading ...</p>
+      </div>
+    </>
+  );
+};
