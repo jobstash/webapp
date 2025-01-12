b637c7274d057916670f6fa2ea55fb02dae92e4e
johnshift <johnshift.dev@gmail.com>
2025-01-12 09:36:07 +0800

    perf: reduce sentry bundle size by 70%!

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,20 @@ const nextConfig: NextConfig = {
       hmrRefreshes: true,
     },
   },
+  webpack: (config, { webpack }) => {
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        __SENTRY_DEBUG__: false,
+        __SENTRY_TRACING__: false,
+        __RRWEB_EXCLUDE_IFRAME__: true,
+        __RRWEB_EXCLUDE_SHADOW_DOM__: true,
+        __SENTRY_EXCLUDE_REPLAY_WORKER__: true,
+      }),
+    );
+
+    // return the modified config
+    return config;
+  },
 };
 
 export default withSentryConfig(nextConfig, {
