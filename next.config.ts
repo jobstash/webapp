fa35efbbc353f23269ec816145c148c78611595f
johnshift <johnshift.dev@gmail.com>
2025-01-12 11:21:25 +0800

    chore: update sentry config to webapp

--- next.config.ts
+++ next.config.ts
@@ -30,7 +30,7 @@ export default withSentryConfig(nextConfig, {
   // https://github.com/getsentry/sentry-webpack-plugin#options
 
   org: 'jobstash',
-  project: 'app',
+  project: 'webapp',
 
   // Only print logs for uploading source maps in CI
   silent: !process.env.CI,
