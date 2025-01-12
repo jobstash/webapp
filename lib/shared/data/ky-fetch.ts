b8c4944df1dfdbe42d19dd48a0c66ab95cb5f466
johnshift <johnshift.dev@gmail.com>
2025-01-12 22:06:11 +0800

    refactor: add sentry breadcrumbs

--- lib/shared/data/ky-fetch.ts
+++ lib/shared/data/ky-fetch.ts
@@ -1,3 +1,4 @@
+import { addBreadcrumb } from '@sentry/nextjs';
 import ky from 'ky';
 
 const DEFAULT_TIMEOUT = 60_000;
@@ -6,12 +7,38 @@ export const kyFetch = ky.create({
   credentials: 'include',
   mode: 'cors',
   timeout: DEFAULT_TIMEOUT,
+  hooks: {
+    beforeRequest: [
+      (req, options) => {
+        addBreadcrumb({
+          category: 'http',
+          message: `kyFetch::beforeRequest`,
+          level: 'info',
+          data: {
+            url: req.url,
+            method: req.method,
+            next: options.next,
+          },
+        });
+      },
+    ],
+    afterResponse: [
+      (req, options, res) => {
+        addBreadcrumb({
+          category: 'http',
+          message: `kyFetch::afterResponse`,
+          level: 'info',
+          data: {
+            url: req.url,
+            status: res.status,
+            next: options.next,
+          },
+        });
+      },
+    ],
+  },
   // headers: {
   //   'ETag': '...',
   //   'If-None-Match': '...'
-  // },
-  // hooks: {
-  //   beforeRequest: [() => console.log('...')],
-  //   afterResponse: [() => console.log('...')],
   // },
 });
