a81e5a267d396f557062148479a24aa5429c529a
johnshift <johnshift.dev@gmail.com>
2025-01-18 19:28:05 +0800

    refactor: moved job-list-page to lib for storybook compatibility
    guard client job-list to render only if ssr data has full PAGE_SIZE items

---
+++ lib/jobs/pages/job-list-page.tsx
@@ -0,0 +1,27 @@
+import { Suspense } from 'react';
+
+import { PAGE_SIZE } from '@/lib/shared/core/envs';
+import { JobListPageSchema } from '@/lib/jobs/core/schemas';
+
+import { JobList } from '@/lib/jobs/ui/job-list';
+import { SsrJobList } from '@/lib/jobs/ui/ssr-job-list';
+
+interface Props {
+  ssrData: JobListPageSchema['data'];
+}
+
+export const JobListPage = ({ ssrData }: Props) => {
+  const showClientJobList = ssrData.length >= PAGE_SIZE;
+  return (
+    <div className='flex w-full justify-center'>
+      <div className='w-full max-w-2xl space-y-16 py-16'>
+        <SsrJobList jobs={ssrData} />
+        {showClientJobList && (
+          <Suspense fallback={<p>LOADING ...</p>}>
+            <JobList />
+          </Suspense>
+        )}
+      </div>
+    </div>
+  );
+};
