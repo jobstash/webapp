4dc6e305f017b4460dad5122936a0de2a11e5835
johnshift <johnshift.dev@gmail.com>
2025-03-18 18:50:14 +0800

    feat(jobs): use job-list server action in swr infinite

--- lib/jobs/ui/job-list.tsx
+++ lib/jobs/ui/job-list.tsx
@@ -4,14 +4,20 @@ import { useInView } from 'react-intersection-observer';
 
 import useSWRInfinite from 'swr/infinite';
 
-import { fetchJobListPage } from '@/lib/jobs/server/data';
-
 import { JobListItem } from '@/lib/jobs/ui/job-list-item';
+
+import { flattenJobItems } from '../utils/flatten-job-items';
+
+import { jobListAction } from '@/lib/jobs/server/actions';
 
 export const JobList = () => {
   const { data, error, isLoading, size, setSize } = useSWRInfinite(
-    (page: number) => ({ page: page + 2 }),
-    ({ page }) => fetchJobListPage({ page }),
+    (pageIndex) => {
+      // Start from page 2 because the first page is SSR'd
+      const page = pageIndex + 2;
+      return { page };
+    },
+    async (args) => jobListAction(args),
     {
       revalidateFirstPage: false,
       shouldRetryOnError: false,
@@ -30,10 +36,13 @@ export const JobList = () => {
   if (isLoading) return <p>TODO: JobList Loading UI</p>;
   if (error) return <p>TODO: JobList Error UI</p>;
 
+  const jobItems = flattenJobItems(data);
+
   return (
     <>
-      {data &&
-        data.flatMap((d) => d.data).map((job) => <JobListItem key={job.id} job={job} />)}
+      {jobItems.map((job) => (
+        <JobListItem key={job.id} job={job} />
+      ))}
       <div className='py-20'>
         <p ref={ref}>Loading ...</p>
       </div>
