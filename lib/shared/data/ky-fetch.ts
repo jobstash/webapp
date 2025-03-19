5fc0310b724bab8bc048eff6f43a200b24ce9651
johnshift <johnshift.dev@gmail.com>
2025-03-19 11:42:59 +0800

    refactor(ky): comment out before/after hooks

--- lib/shared/data/ky-fetch.ts
+++ lib/shared/data/ky-fetch.ts
@@ -1,4 +1,3 @@
-import { addBreadcrumb } from '@sentry/nextjs';
 import ky from 'ky';
 
 const DEFAULT_TIMEOUT = 60_000;
@@ -8,36 +7,36 @@ export const kyFetch = ky.create({
   mode: 'cors',
   timeout: DEFAULT_TIMEOUT,
   cache: 'no-store',
-  hooks: {
-    beforeRequest: [
-      (req, options) => {
-        addBreadcrumb({
-          category: 'http',
-          message: `kyFetch::beforeRequest`,
-          level: 'info',
-          data: {
-            url: req.url,
-            method: req.method,
-            next: options.next,
-          },
-        });
-      },
-    ],
-    afterResponse: [
-      (req, options, res) => {
-        addBreadcrumb({
-          category: 'http',
-          message: `kyFetch::afterResponse`,
-          level: 'info',
-          data: {
-            url: req.url,
-            status: res.status,
-            next: options.next,
-          },
-        });
-      },
-    ],
-  },
+  // hooks: {
+  //   beforeRequest: [
+  //     (req, options) => {
+  //       addBreadcrumb({
+  //         category: 'http',
+  //         message: `kyFetch::beforeRequest`,
+  //         level: 'info',
+  //         data: {
+  //           url: req.url,
+  //           method: req.method,
+  //           next: options.next,
+  //         },
+  //       });
+  //     },
+  //   ],
+  //   afterResponse: [
+  //     (req, options, res) => {
+  //       addBreadcrumb({
+  //         category: 'http',
+  //         message: `kyFetch::afterResponse`,
+  //         level: 'info',
+  //         data: {
+  //           url: req.url,
+  //           status: res.status,
+  //           next: options.next,
+  //         },
+  //       });
+  //     },
+  //   ],
+  // },
   // headers: {
   //   'ETag': '...',
   //   'If-None-Match': '...'
