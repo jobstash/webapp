af5be1b282f014140f2c35e98b7114f3514ee427
johnshift <johnshift.dev@gmail.com>
2025-03-19 12:22:54 +0800

    feat: setup shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@/lib/shared/ui/base/button';
+
 export default function Home() {
   return (
     <div>
       <p>Hello</p>
+      <Button>Hello Shadcn</Button>
     </div>
   );
 }
