5fcaa24e30423ac1a6bfe659c599f8d8766c2cb5
johnshift <johnshift.dev@gmail.com>
2025-03-19 19:52:36 +0800

    style(jobs): adjust job-list layout spacing

--- lib/jobs/ui/job-list-item.tsx
+++ lib/jobs/ui/job-list-item.tsx
@@ -8,7 +8,7 @@ export const JobListItem = ({ job }: Props) => {
   const { id, title, infoTags, tags } = job;
 
   return (
-    <div className='flex w-full flex-col gap-4 rounded-xl border border-neutral-600 p-4'>
+    <div className='flex w-full flex-col gap-4 rounded-3xl border border-neutral-600 p-6'>
       <p>{id}</p>
       <p>{title}</p>
       <pre>
