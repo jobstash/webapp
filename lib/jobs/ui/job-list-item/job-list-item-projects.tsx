1a80221e6e61379d5fc6d38bb5acc03f1db7cd61
johnshift <johnshift.dev@gmail.com>
2025-03-20 16:10:35 +0800

    feat(jobs): add job-list-item projects component

---
+++ lib/jobs/ui/job-list-item/job-list-item-projects.tsx
@@ -0,0 +1,28 @@
+import { Fragment } from 'react';
+
+import { JobListItemSchema } from '@/lib/jobs/core/schemas';
+
+import { Divider } from '@/lib/shared/ui/divider';
+import { InfoTags } from '@/lib/shared/ui/info-tags';
+import { LogoTitle } from '@/lib/shared/ui/logo-title';
+import { jobProjectInfoTagsMap } from '@/lib/jobs/ui/tag-icon-map';
+
+interface Props {
+  projects: JobListItemSchema['projects'];
+}
+
+export const JobListItemProjects = ({ projects }: Props) => {
+  return projects.map(({ name, logo, infoTags }) => (
+    <Fragment key={name}>
+      <Divider />
+      <div className='flex flex-col gap-4'>
+        <div key={name} className='flex items-center gap-4'>
+          <LogoTitle size='sm' title={name} src={logo ?? ''} />
+          {infoTags.length > 0 && (
+            <InfoTags iconMap={jobProjectInfoTagsMap} infoTags={infoTags} />
+          )}
+        </div>
+      </div>
+    </Fragment>
+  ));
+};
