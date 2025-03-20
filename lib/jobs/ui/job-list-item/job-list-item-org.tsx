2674afe1ff47fd98ea6e24d96b857ba0af5c7df2
johnshift <johnshift.dev@gmail.com>
2025-03-20 16:09:56 +0800

    feat(jobs): add job-list-item org component

---
+++ lib/jobs/ui/job-list-item/job-list-item-org.tsx
@@ -0,0 +1,26 @@
+import { JobListItemSchema } from '@/lib/jobs/core/schemas';
+
+import { Divider } from '@/lib/shared/ui/divider';
+import { InfoTags } from '@/lib/shared/ui/info-tags';
+import { LogoTitle } from '@/lib/shared/ui/logo-title';
+import { jobOrgInfoTagsMap } from '@/lib/jobs/ui/tag-icon-map';
+
+interface Props {
+  org: JobListItemSchema['organization'];
+}
+
+export const JobListItemOrg = ({ org }: Props) => {
+  if (!org) return null;
+
+  const { name, location, logo, infoTags } = org;
+
+  return (
+    <>
+      <Divider />
+      <div className='flex items-center gap-4'>
+        <LogoTitle size='sm' title={name} subtitle={location} src={logo ?? ''} />
+        {infoTags && <InfoTags iconMap={jobOrgInfoTagsMap} infoTags={infoTags} />}
+      </div>
+    </>
+  );
+};
