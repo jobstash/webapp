88301615e11109636eaa89f5d36adb97eb233976
johnshift <johnshift.dev@gmail.com>
2025-03-20 17:36:42 +0800

    feat(shared): add brand component

---
+++ lib/shared/ui/brand.tsx
@@ -0,0 +1,29 @@
+import Link from 'next/link';
+
+import { JobstashLogo } from '@/lib/shared/ui/svgs/jobstash-logo';
+
+const BRAND_NAME = 'JobStash';
+const VERI_URL = 'https://ecosystem.vision';
+const VERI_NAME = 'veri.xyz';
+
+export const Brand = () => {
+  return (
+    <div className='flex h-16 items-center gap-3 px-4'>
+      <JobstashLogo className='size-12 shrink-0' />
+      <div className='flex flex-col justify-center'>
+        <span className='text-xl leading-5 font-bold'>{BRAND_NAME}</span>
+        <span className='pl-0.5 text-xs leading-snug text-white/40'>
+          by
+          <Link
+            href={VERI_URL}
+            target='_blank'
+            rel='noopener noreferrer'
+            className='pl-1 hover:underline'
+          >
+            {VERI_NAME}
+          </Link>
+        </span>
+      </div>
+    </div>
+  );
+};
