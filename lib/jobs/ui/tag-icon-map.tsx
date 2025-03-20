884c4ab10a543e9f45080f62dccf2c7ab43e38a0
johnshift <johnshift.dev@gmail.com>
2025-03-20 16:01:03 +0800

    feat(jobs): add job project info-tag icon mapping

--- lib/jobs/ui/tag-icon-map.tsx
+++ lib/jobs/ui/tag-icon-map.tsx
@@ -1,11 +1,16 @@
+import { AuditIcon } from '@/lib/shared/ui/svgs/audit-icon';
 import { BankIcon } from '@/lib/shared/ui/svgs/bank-icon';
 import { CategoryIcon } from '@/lib/shared/ui/svgs/category-icon';
 import { CommitmentIcon } from '@/lib/shared/ui/svgs/commitment-icon';
+import { HackIcon } from '@/lib/shared/ui/svgs/hack-icon';
 import { LocationIcon } from '@/lib/shared/ui/svgs/location-icon';
+import { MonthlyVolumeIcon } from '@/lib/shared/ui/svgs/monthly-volume-icon';
 import { PaysInCryptoIcon } from '@/lib/shared/ui/svgs/pays-in-crypto-icon';
 import { SalaryIcon } from '@/lib/shared/ui/svgs/salary-icon';
 import { SeniorityIcon } from '@/lib/shared/ui/svgs/seniority-icon';
 import { TokenAllocationIcon } from '@/lib/shared/ui/svgs/token-allocation-icon';
+import { TokenIcon } from '@/lib/shared/ui/svgs/token-icon';
+import { TvlIcon } from '@/lib/shared/ui/svgs/tvl-icon';
 import { UsersIcon } from '@/lib/shared/ui/svgs/users-icon';
 import { WorkModeIcon } from '@/lib/shared/ui/svgs/work-mode-icon';
 
@@ -27,19 +32,13 @@ export const jobOrgInfoTagsMap: Record<string, React.ReactNode> = {
 };
 
 export const jobProjectInfoTagsMap: Record<string, React.ReactNode> = {
-  tokenSymbol: null,
-  category: null,
-};
-
-export const jobProjectTvlTagsMap: Record<string, React.ReactNode> = {
-  tvl: null,
-  monthlyVolume: null,
-  monthlyActiveUsers: null,
-  monthlyFees: null,
-  monthlyRevenue: null,
-};
-
-export const jobProjectAuditTagsMap: Record<string, React.ReactNode> = {
-  audit: null,
-  hack: null,
+  tokenSymbol: <TokenIcon />,
+  category: <CategoryIcon />,
+  tvl: <TvlIcon />,
+  monthlyVolume: <MonthlyVolumeIcon />,
+  monthlyActiveUsers: <UsersIcon />,
+  monthlyFees: <MonthlyVolumeIcon />,
+  monthlyRevenue: <MonthlyVolumeIcon />,
+  audit: <AuditIcon />,
+  hack: <HackIcon />,
 };
