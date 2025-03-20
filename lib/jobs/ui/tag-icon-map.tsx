f78ea90d9e4b08f8af0ae7a18b0884fcd9fe3169
johnshift <johnshift.dev@gmail.com>
2025-03-20 12:10:06 +0800

    feat(jobs): add info-tag icon mapping

--- lib/jobs/ui/tag-icon-map.tsx
+++ lib/jobs/ui/tag-icon-map.tsx
@@ -1,12 +1,21 @@
+import { CategoryIcon } from '@/lib/shared/ui/svgs/category-icon';
+import { CommitmentIcon } from '@/lib/shared/ui/svgs/commitment-icon';
+import { LocationIcon } from '@/lib/shared/ui/svgs/location-icon';
+import { PaysInCryptoIcon } from '@/lib/shared/ui/svgs/pays-in-crypto-icon';
+import { SalaryIcon } from '@/lib/shared/ui/svgs/salary-icon';
+import { SeniorityIcon } from '@/lib/shared/ui/svgs/seniority-icon';
+import { TokenAllocationIcon } from '@/lib/shared/ui/svgs/token-allocation-icon';
+import { WorkModeIcon } from '@/lib/shared/ui/svgs/work-mode-icon';
+
 export const jobInfoTagsMap: Record<string, React.ReactNode> = {
-  seniority: null,
-  salary: null,
-  location: null,
-  workMode: null,
-  commitment: null,
-  paysInCrypto: null,
-  offersTokenAllocation: null,
-  classification: null,
+  seniority: <SeniorityIcon />,
+  salary: <SalaryIcon />,
+  location: <LocationIcon />,
+  workMode: <WorkModeIcon />,
+  commitment: <CommitmentIcon />,
+  paysInCrypto: <PaysInCryptoIcon />,
+  offersTokenAllocation: <TokenAllocationIcon />,
+  category: <CategoryIcon />,
 };
 
 export const jobProjectInfoTagsMap: Record<string, React.ReactNode> = {
