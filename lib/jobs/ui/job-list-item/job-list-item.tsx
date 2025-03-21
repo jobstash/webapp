674c25f69eb0b2804b547964f44b8c8079db7734
johnshift <johnshift.dev@gmail.com>
2025-03-21 08:16:27 +0800

    feat(jobs): add animated border to featured jobs

--- lib/jobs/ui/job-list-item/job-list-item.tsx
+++ lib/jobs/ui/job-list-item/job-list-item.tsx
@@ -1,5 +1,6 @@
 import { JobListItemSchema } from '@/lib/jobs/core/schemas';
 
+import { BorderBeam } from '@/lib/shared/ui/base/border-beam';
 import { InfoTags } from '@/lib/shared/ui/info-tags';
 import { jobInfoTagsMap } from '@/lib/jobs/ui/tag-icon-map';
 
@@ -11,15 +12,23 @@ interface Props {
 }
 
 export const JobListItem = ({ job }: Props) => {
-  const { title, infoTags, tags, organization, projects } = job;
+  const {
+    title,
+    infoTags,
+    tags,
+    organization,
+    projects,
+    promotion: { isFeatured },
+  } = job;
 
   return (
-    <article className='flex w-full flex-col gap-4 rounded-3xl border border-neutral-800/80 bg-sidebar p-6'>
+    <article className='relative flex w-full flex-col gap-4 rounded-3xl border border-neutral-800/80 bg-sidebar p-6'>
       <h2 className='text-xl font-bold'>{title}</h2>
       <InfoTags iconMap={jobInfoTagsMap} infoTags={infoTags} />
       <JobListItemOrg org={organization} />
       <JobListItemTags tags={tags} />
       <JobListItemProjects projects={projects} />
+      {isFeatured && <BorderBeam size={320} duration={12} delay={9} />}
     </article>
   );
 };
