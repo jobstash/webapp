7f5ee3c5352c409a3509810b7e752df72d8ccecd
johnshift <johnshift.dev@gmail.com>
2025-03-21 09:40:21 +0800

    refactor(sentry): add local guard on client config

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -49,7 +49,10 @@ const client = new BrowserClient({
   },
 });
 
-if (process.env.NODE_ENV === 'production') {
+if (
+  process.env.NODE_ENV === 'production' &&
+  process.env.NEXT_PUBLIC_IS_LOCAL !== 'true'
+) {
   getCurrentScope().setClient(client);
   client.init();
 }
