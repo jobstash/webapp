e8a257914a53cfcea6a3f2789a44d84cbf3e7a0f
johnshift <johnshift.dev@gmail.com>
2025-03-25 10:30:42 +0800

    feat(app): setup nuqs

--- lib/shared/providers/root-providers.tsx
+++ lib/shared/providers/root-providers.tsx
@@ -1,11 +1,13 @@
 'use client';
 
+import { NuqsAdapter } from 'nuqs/adapters/next/app';
+
 import { ThemeProvider } from './theme-provider';
 
 export const RootProviders = ({ children }: React.PropsWithChildren) => {
   return (
     <ThemeProvider attribute='class' defaultTheme='dark' disableTransitionOnChange>
-      {children}
+      <NuqsAdapter>{children}</NuqsAdapter>
     </ThemeProvider>
   );
 };
