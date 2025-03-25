1dbba53d135f34626b6d6cefb1e8c723113baca7
johnshift <johnshift.dev@gmail.com>
2025-03-25 12:23:36 +0800

    refactor(sidebar): remove old app-aside components and add new sidebar structure

--- lib/shared/ui/sidebar/sidebar.tsx
+++ lib/shared/ui/sidebar/sidebar.tsx
@@ -7,7 +7,7 @@ import { SocialsSidebar } from './socials-sidebar';
 /**
  * A sidebar component that appears on the left side of the layout
  */
-export const AppSidebar = () => {
+export const Sidebar = () => {
   return (
     <div className='flex flex-col gap-6'>
       <Brand />
