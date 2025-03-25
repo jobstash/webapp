86e208ee7a27ef0331580dacf0c149a67fbeff16
johnshift <johnshift.dev@gmail.com>
2025-03-25 10:26:45 +0800

    feat(filters): add advanced filter toggler

---
+++ lib/shared/ui/app-aside/filters-aside/advanced-filters.tsx
@@ -0,0 +1,7 @@
+export const AdvancedFilters = () => {
+  return (
+    <div className='my-4 h-[600px] min-h-0 flex-1 overflow-y-auto rounded-xl bg-muted p-4'>
+      AdvancedFilters
+    </div>
+  );
+};
