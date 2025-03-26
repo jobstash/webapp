ffa258a2c37442bb94ef3d73ec2ed2ef4185a53b
johnshift <johnshift.dev@gmail.com>
2025-03-26 14:18:15 +0800

    feat(jobs): init job-details aside

--- app/[title]/[id]/layout.tsx
+++ app/[title]/[id]/layout.tsx
@@ -1,6 +1,14 @@
 import { SidebarAsideLayout } from '@/lib/shared/layouts/sidebar-aside-layout';
 
+const DetailsAside = () => {
+  return (
+    <div className='flex flex-col gap-8 rounded-2xl bg-sidebar p-6'>
+      <div className='h-[600px] rounded-2xl bg-muted' />
+    </div>
+  );
+};
+
 const Layout = ({ children }: React.PropsWithChildren) => {
-  return <SidebarAsideLayout>{children}</SidebarAsideLayout>;
+  return <SidebarAsideLayout aside={<DetailsAside />}>{children}</SidebarAsideLayout>;
 };
 export default Layout;
