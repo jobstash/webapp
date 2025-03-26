4317c9f6ac6e263ae7ec562d08cdb7e39fd936f0
johnshift <johnshift.dev@gmail.com>
2025-03-26 13:58:39 +0800

    feat(jobs): add cache on job-list and job-details fetch

--- app/[title]/[id]/page.tsx
+++ app/[title]/[id]/page.tsx
@@ -1,9 +1,6 @@
 import { fetchJobDetails } from '@/lib/jobs/server/data/fetch-job-details';
 import { fetchJobDetailsStaticParams } from '@/lib/jobs/server/data/fetch-job-details-static-params';
 
-// Routes not generated at build time will be 404
-export const dynamicParams = false;
-
 export const generateStaticParams = async () => {
   return fetchJobDetailsStaticParams();
 };
