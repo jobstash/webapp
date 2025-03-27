1b6329cf9de96b73bd75b8ad0d7846b580ed3433
johnshift <johnshift.dev@gmail.com>
2025-03-27 11:39:33 +0800

    feat(app): add link to brand

--- lib/shared/ui/brand.tsx
+++ lib/shared/ui/brand.tsx
@@ -10,9 +10,13 @@ const VERI_LABEL = 'veri.xyz';
 export const Brand = () => {
   return (
     <div className='flex h-16 items-center gap-3 px-4'>
-      <JobstashLogo className='size-12 shrink-0' />
+      <Link href={envs.FRONTEND_URL}>
+        <JobstashLogo className='size-12 shrink-0' />
+      </Link>
       <div className='flex flex-col justify-center'>
-        <span className='text-xl leading-5 font-bold'>{BRAND_NAME}</span>
+        <Link href={envs.FRONTEND_URL} className='text-xl leading-5 font-bold'>
+          {BRAND_NAME}
+        </Link>
         <span className='pl-0.5 text-xs leading-snug text-white/40'>
           by
           <Link
