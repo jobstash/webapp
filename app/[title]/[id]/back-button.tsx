5093623a76b8cfd69a6a974e263dcc6be8cbec3e
johnshift <johnshift.dev@gmail.com>
2025-03-27 09:24:48 +0800

    fix(header): resolve issue where header has weird animation during back navigation

--- app/[title]/[id]/back-button.tsx
+++ app/[title]/[id]/back-button.tsx
@@ -5,11 +5,19 @@ import { useRouter } from 'next/navigation';
 import { ArrowLeftIcon } from 'lucide-react';
 
 import { Button } from '@/lib/shared/ui/base/button';
+import { useHeaderStore } from '@/lib/filters/ui/header/store';
 
 export const BackButton = () => {
   const router = useRouter();
+  const setIsExpanded = useHeaderStore((state) => state.setIsExpanded);
+
+  const onClick = () => {
+    setIsExpanded(false);
+    router.back();
+  };
+
   return (
-    <Button variant='secondary' size='icon' onClick={router.back}>
+    <Button variant='secondary' size='icon' onClick={onClick}>
       <ArrowLeftIcon className='h-4 w-4' />
     </Button>
   );
