2936bf1351d898232c987d3431986e81f4246cb2
johnshift <johnshift.dev@gmail.com>
2025-03-29 20:31:11 +0800

    refactor(app): remove bs - moved to metadata instead

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,3 +1,4 @@
+import { Metadata } from 'next';
 import { Suspense } from 'react';
 
 import { FiltersAside } from '@/lib/filters/ui/filters-aside';
@@ -5,6 +6,12 @@ import { FiltersAsideSkeleton } from '@/lib/filters/ui/filters-aside';
 import { AppHeader } from '@/lib/filters/ui/header';
 
 import { SidebarAsideLayout } from '@/lib/shared/layouts/sidebar-aside-layout';
+
+export const metadata: Metadata = {
+  title: 'Crypto Native Jobs',
+  description:
+    'Explore crypto native jobs across the entire crypto ecosystem, powered by AI and enhanced by unique data insights as a public good.',
+};
 
 const Layout = ({ children }: React.PropsWithChildren) => {
   return (
