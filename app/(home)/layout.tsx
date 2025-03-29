ecea51e9c46cf09aea7558cd2c561b7bc5446478
johnshift <johnshift.dev@gmail.com>
2025-03-29 20:04:48 +0800

    refactor(app): move header from page to layout

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -2,6 +2,7 @@ import { Suspense } from 'react';
 
 import { FiltersAside } from '@/lib/filters/ui/filters-aside';
 import { FiltersAsideSkeleton } from '@/lib/filters/ui/filters-aside';
+import { AppHeader } from '@/lib/filters/ui/header';
 
 import { SidebarAsideLayout } from '@/lib/shared/layouts/sidebar-aside-layout';
 
@@ -14,6 +15,7 @@ const Layout = ({ children }: React.PropsWithChildren) => {
         </Suspense>
       }
     >
+      <AppHeader />
       {children}
     </SidebarAsideLayout>
   );
