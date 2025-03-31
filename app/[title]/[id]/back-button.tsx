20a3b5dbc965303067f79bee871353283d02cb18
johnshift <johnshift.dev@gmail.com>
2025-03-31 10:37:35 +0800

    fix(home): back-button build error

--- app/[title]/[id]/back-button.tsx
+++ app/[title]/[id]/back-button.tsx
@@ -5,19 +5,12 @@ import { useRouter } from 'next/navigation';
 import { ArrowLeftIcon } from 'lucide-react';
 
 import { Button } from '@/lib/shared/ui/base/button';
-import { useHeaderStore } from '@/lib/filters/ui/header/store';
 
 export const BackButton = () => {
   const router = useRouter();
-  const setIsExpanded = useHeaderStore((state) => state.setIsExpanded);
-
-  const onClick = () => {
-    setIsExpanded(false);
-    router.back();
-  };
 
   return (
-    <Button variant='secondary' size='icon' onClick={onClick}>
+    <Button variant='secondary' size='icon' onClick={router.back}>
       <ArrowLeftIcon className='h-4 w-4' />
     </Button>
   );
