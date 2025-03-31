174353b05330abfc537b2bdbc44bf679784653a9
johnshift <johnshift.dev@gmail.com>
2025-03-31 12:41:48 +0800

    feat(header): init full page filter overlay

--- lib/filters/ui/full-page-filters-overlay.tsx
+++ lib/filters/ui/full-page-filters-overlay.tsx
@@ -1,28 +1,19 @@
 'use client';
 
-import { ListFilterIcon } from 'lucide-react';
-
 import { FULL_PAGE_OVERLAYS } from '@/lib/shared/core/constants';
 import { useFullPageOverlayStore } from '@/lib/shared/core/store';
 
-import { Button } from '@/lib/shared/ui/base/button';
 import { FullPageOverlay } from '@/lib/shared/ui/full-page-overlay';
 
-export const FullPageSearchOverlay = () => {
+export const FullPageFiltersOverlay = () => {
   const active = useFullPageOverlayStore((state) => state.active);
-  const isOpen = active === FULL_PAGE_OVERLAYS.SEARCH;
+  const isOpen = active === FULL_PAGE_OVERLAYS.FILTERS;
   const close = useFullPageOverlayStore((state) => state.close);
-
-  const open = useFullPageOverlayStore((state) => state.open);
-  const openFilters = () => open(FULL_PAGE_OVERLAYS.FILTERS);
 
   return (
     <FullPageOverlay isOpen={isOpen} onClose={close}>
       <div className='grid size-full place-items-center'>
         <p>TODO</p>
-        <Button onClick={openFilters} variant='secondary' size='icon'>
-          <ListFilterIcon className='size-5' />
-        </Button>
       </div>
     </FullPageOverlay>
   );
