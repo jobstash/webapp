71b9d6ef873f96219f9cd3ff7b2f8bcf70886e4a
johnshift <johnshift.dev@gmail.com>
2025-04-01 17:51:22 +0800

    feat(filters): integrate nuqs to checkbox-filter component

--- lib/filters/ui/checkbox-filter/checkbox-filter.tsx
+++ lib/filters/ui/checkbox-filter/checkbox-filter.tsx
@@ -1,22 +1,32 @@
+'use client';
+
 import { CheckboxFilterConfigSchema } from '@/lib/filters/core/schemas';
 
 import { Checkbox } from '@/lib/shared/ui/base/checkbox';
 import { Label } from '@/lib/shared/ui/base/label';
 
-import { FilterAccordionItem } from './filter-accordion-item';
+import { FilterAccordionItem } from '../filter-accordion-item';
+
+import { useCheckboxFilter } from './use-checkbox-filter';
 
 interface Props {
   config: CheckboxFilterConfigSchema;
 }
 
 export const CheckboxFilter = ({ config }: Props) => {
-  const { options } = config;
+  const { options, paramKey, label } = config;
+  const { isItemChecked, toggleItem } = useCheckboxFilter(paramKey);
+
   return (
-    <FilterAccordionItem label={config.label}>
+    <FilterAccordionItem label={label}>
       <div className='flex flex-col gap-2'>
         {options.map(({ value, label }) => (
-          <div key={value} className='flex items-center space-x-2'>
-            <Checkbox id={value} value={value} />
+          <div key={label} className='flex items-center space-x-2'>
+            <Checkbox
+              value={value}
+              checked={isItemChecked(value)}
+              onCheckedChange={(checked) => toggleItem(value, checked)}
+            />
             <Label htmlFor={value} className='text-muted-foreground'>
               {label}
             </Label>
