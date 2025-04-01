bb9aac82dce63587720e98e0909c80555e55a421
johnshift <johnshift.dev@gmail.com>
2025-04-01 20:49:02 +0800

    refactor(jobs): migrate from swr to react-query

---
+++ lib/jobs/ui/job-list/use-job-list.ts
@@ -0,0 +1,36 @@
+import { useInView } from 'react-intersection-observer';
+
+import { useJobListQuery } from './use-job-list-query';
+
+export const useJobList = (startPage?: number) => {
+  const {
+    isLoading,
+    isFetchingNextPage: isLoadingNextPage,
+    isError,
+    fetchNextPage,
+    data,
+    hasNextPage,
+  } = useJobListQuery(startPage);
+
+  const { ref: inViewRef } = useInView({
+    threshold: 1,
+    onChange: (inView) => {
+      if (inView && !isLoading && hasNextPage) {
+        fetchNextPage();
+      }
+    },
+  });
+
+  const items = data?.pages.flatMap((page) => page.data) ?? [];
+  const isListIndicatorVisible = items.length > 0;
+
+  return {
+    isLoading,
+    isLoadingNextPage,
+    isError,
+    inViewRef,
+    items,
+    isListIndicatorVisible,
+    isEndReached: !hasNextPage,
+  };
+};
