ca889f0055020ffb25f1af3ff66dc5afeb3b8d77
johnshift <johnshift.dev@gmail.com>
2025-04-01 17:53:49 +0800

    feat(jobs): update job list page to conditionally fetch data based on search parameters

--- lib/jobs/ui/job-list-skeleton.tsx
+++ lib/jobs/ui/job-list-skeleton.tsx
@@ -1,9 +1,11 @@
+import { ClassValue } from 'clsx';
+
 import { cn } from '@/lib/shared/utils';
 
 interface Props {
-  className?: string;
+  className?: ClassValue;
 }
 
 export const JobListSkeleton = ({ className }: Props) => {
-  return <div className={cn('py-20', className)}>{'TODO: <JobListSkeleton />'}</div>;
+  return <div className={cn(className)}>{'TODO: <JobListSkeleton />'}</div>;
 };
