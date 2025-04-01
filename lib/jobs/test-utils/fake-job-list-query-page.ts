97b25a0db8b42dd7ec7775b3c0f16f447136c40a
johnshift <johnshift.dev@gmail.com>
2025-04-01 22:02:35 +0800

    fix: build error

--- lib/jobs/test-utils/fake-job-list-query-page.ts
+++ lib/jobs/test-utils/fake-job-list-query-page.ts
@@ -6,13 +6,14 @@ import { JobListPageSchema } from '@/lib/jobs/core/schemas';
 import { fakeJobItem } from '@/lib/jobs/test-utils/fake-job-item';
 
 export const fakeJobListQueryPage = (override?: Partial<JobListPageSchema>) => {
-  const { page: overridePage, ...rest } = override ?? {};
+  const { page: overridePage, hasNextPage, ...rest } = override ?? {};
   const page = overridePage ?? faker.number.int({ min: 1, max: 100 });
   faker.seed(page);
   return {
     page,
     total: faker.number.int({ min: 1, max: 5000 }),
     data: Array.from({ length: ENV.PAGE_SIZE }, fakeJobItem),
+    hasNextPage: hasNextPage ?? true,
     ...rest,
   };
 };
