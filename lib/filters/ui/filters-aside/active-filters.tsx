b539969f496fc30b47fd2284d33095b2a3c17e05
johnshift <johnshift.dev@gmail.com>
2025-04-02 18:19:04 +0800

    feat(filters): integrate url state to active filter items

--- lib/filters/ui/filters-aside/active-filters.tsx
+++ lib/filters/ui/filters-aside/active-filters.tsx
@@ -1,13 +1,15 @@
 'use client';
-import { FilterItemConfigMapper } from './filter-item-config-mapper';
-import { useFilterStore } from './store';
+
+import { useFilterStore } from '@/lib/filters/core/store';
+
+import { ActiveFilterItem } from '@/lib/filters/ui/filters-aside/active-filter-item';
 
 export const ActiveFilters = () => {
   const activeFilters = useFilterStore((state) => state.activeFilters);
   return (
     <>
       {activeFilters.map((config) => (
-        <FilterItemConfigMapper key={config.label} config={config} />
+        <ActiveFilterItem key={config.label} config={config} />
       ))}
     </>
   );
--- lib/filters/ui/filters-aside/index.ts
+++ lib/filters/ui/filters-aside/index.ts
@@ -1,3 +1,2 @@
-export * from './active-filter-item';
 export * from './filters-aside';
 export * from './filters-aside-skeleton';
