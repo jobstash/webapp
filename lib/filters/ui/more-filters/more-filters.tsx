06ff86fe6753ec64bf13254f58ee45ed862f357c
johnshift <johnshift.dev@gmail.com>
2025-04-03 11:10:50 +0800

    refactor(filters): reuse `FilterItemPopover` for `MoreFilters` component

--- lib/filters/ui/more-filters/more-filters.tsx
+++ lib/filters/ui/more-filters/more-filters.tsx
@@ -1,7 +1,5 @@
 'use client';
 
-import { useState } from 'react';
-
 import { ListFilterPlusIcon } from 'lucide-react';
 
 import { FilterConfigItemSchema } from '@/lib/filters/core/schemas';
@@ -15,7 +13,7 @@ import {
   CommandInput,
   CommandList,
 } from '@/lib/shared/ui/base/command';
-import { Popover, PopoverContent, PopoverTrigger } from '@/lib/shared/ui/base/popover';
+import { FilterItemPopover } from '@/lib/filters/ui/filter-item-popover';
 
 import { MoreFiltersItem } from './more-filters-item';
 
@@ -24,35 +22,29 @@ interface Props {
 }
 
 export const MoreFilters = ({ filters }: Props) => {
-  const [open, setOpen] = useState(false);
-  const onClose = () => setOpen(false);
   const activeLabels = useFilterStore((state) => state.activeLabels);
   const options = filters.filter((config) => !activeLabels.has(config.label));
+
   return (
-    <Popover open={open} onOpenChange={setOpen}>
-      <PopoverTrigger asChild>
+    <FilterItemPopover
+      trigger={
         <Button size='xs' variant='ghost' className='h-7 text-muted-foreground'>
           <ListFilterPlusIcon className='size-4' />
           More Filters
         </Button>
-      </PopoverTrigger>
-      <PopoverContent
-        side='bottom'
-        align='start'
-        className='w-48 min-w-60 bg-sidebar p-0'
-      >
-        <Command>
-          <CommandInput placeholder='Search filters...' />
-          <CommandList>
-            <CommandEmpty>No results found.</CommandEmpty>
-            <CommandGroup>
-              {options.map((config) => (
-                <MoreFiltersItem key={config.label} config={config} onClose={onClose} />
-              ))}
-            </CommandGroup>
-          </CommandList>
-        </Command>
-      </PopoverContent>
-    </Popover>
+      }
+    >
+      <Command>
+        <CommandInput placeholder='Search filters...' />
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          <CommandGroup>
+            {options.map((config) => (
+              <MoreFiltersItem key={config.label} config={config} />
+            ))}
+          </CommandGroup>
+        </CommandList>
+      </Command>
+    </FilterItemPopover>
   );
 };
