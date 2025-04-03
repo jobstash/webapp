b7c225a75666df47912cf041623ca4c5aede0939
johnshift <johnshift.dev@gmail.com>
2025-04-03 11:15:37 +0800

    refactor(filters): replace button with filter dropdown mapper in active filter item

--- lib/filters/ui/active-filter-item/active-filter-item.tsx
+++ lib/filters/ui/active-filter-item/active-filter-item.tsx
@@ -1,11 +1,9 @@
 'use client';
 
-import { ChevronDownIcon } from 'lucide-react';
-
 import { FILTER_KIND } from '@/lib/filters/core/constants';
 import { FilterConfigItemSchema } from '@/lib/filters/core/schemas';
 
-import { Button } from '@/lib/shared/ui/base/button';
+import { FilterDropdownMapper } from '@/lib/filters/ui/active-filter-item/filter-dropdown-mapper';
 
 import { MainButton } from './main-button';
 import { useInitFilterParams } from './use-init-filter-params';
@@ -16,7 +14,7 @@ interface Props {
 
 export const ActiveFilterItem = ({ config }: Props) => {
   const isSwitch = config.kind === FILTER_KIND.SWITCH;
-  useInitFilterParams(config);
+  const { filterParamValue } = useInitFilterParams(config);
 
   return (
     <div className='flex overflow-hidden rounded-md'>
@@ -26,14 +24,7 @@ export const ActiveFilterItem = ({ config }: Props) => {
           <div className='flex items-center bg-secondary'>
             <div className='h-4 w-px bg-neutral-600/60'></div>
           </div>
-          <Button
-            size='xs'
-            variant='secondary'
-            className='flex h-7 items-center gap-1.5 rounded-l-none border-l-0 px-2 text-sm hover:bg-neutral-700/50'
-          >
-            TODO
-            <ChevronDownIcon className='size-3.5 text-neutral-400' />
-          </Button>
+          <FilterDropdownMapper filterParamValue={filterParamValue} config={config} />
         </>
       )}
     </div>
