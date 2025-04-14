737b0dad89b9487759c18991636e9bc34249ccbc
johnshift <johnshift.dev@gmail.com>
2025-04-14 13:13:54 +0800

    refactor(search): move search-input to own component and import as props to header

--- lib/shared/ui/header/desktop-header.tsx
+++ lib/shared/ui/header/desktop-header.tsx
@@ -1,18 +1,13 @@
-import { SearchIcon } from 'lucide-react';
-
 import { Button } from '@/lib/shared/ui/base/button';
-import { Input } from '@/lib/shared/ui/base/input';
 
-export const DesktopHeader = () => {
+interface Props {
+  searchInput: React.ReactNode;
+}
+
+export const DesktopHeader = ({ searchInput }: Props) => {
   return (
     <div className='hidden h-16 w-full items-center justify-between p-4 md:flex'>
-      <div className='relative flex w-full pr-4 md:block'>
-        <SearchIcon className='absolute top-1/2 left-3 h-5 w-5 -translate-y-1/2 text-zinc-500' />
-        <Input
-          className='h-10 w-full rounded-lg bg-sidebar pl-10 text-sm'
-          placeholder='Search 3129 jobs'
-        />
-      </div>
+      {searchInput}
       <div className='hidden items-center md:flex'>
         <Button variant='ghost' size='sm'>
           Get Listed
--- lib/shared/ui/header/header.tsx
+++ lib/shared/ui/header/header.tsx
@@ -1,11 +1,15 @@
 import { DesktopHeader } from './desktop-header';
 import { MobileHeader } from './mobile-header';
 
-export const Header = () => {
+interface Props {
+  searchInput: React.ReactNode;
+}
+
+export const Header = ({ searchInput }: Props) => {
   return (
     <div className='sticky top-0 z-30 w-full overflow-hidden border border-neutral-800/50 bg-sidebar/30 backdrop-blur-md lg:top-6 lg:mt-6 lg:rounded-2xl'>
       <MobileHeader />
-      <DesktopHeader />
+      <DesktopHeader searchInput={searchInput} />
     </div>
   );
 };
