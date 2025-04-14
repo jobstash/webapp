136553d0c75ce77f3d65165913d2387e04df2eeb
johnshift <johnshift.dev@gmail.com>
2025-04-14 13:14:51 +0800

    refactor(header): temp hide cta's

--- lib/shared/ui/header/desktop-header.tsx
+++ lib/shared/ui/header/desktop-header.tsx
@@ -1,4 +1,4 @@
-import { Button } from '@/lib/shared/ui/base/button';
+// import { Button } from '@/lib/shared/ui/base/button';
 
 interface Props {
   searchInput: React.ReactNode;
@@ -8,14 +8,14 @@ export const DesktopHeader = ({ searchInput }: Props) => {
   return (
     <div className='hidden h-16 w-full items-center justify-between p-4 md:flex'>
       {searchInput}
-      <div className='hidden items-center md:flex'>
+      {/* <div className='hidden items-center md:flex'>
         <Button variant='ghost' size='sm'>
           Get Listed
         </Button>
         <Button variant='ghost' size='sm'>
           Subscribe on TG
         </Button>
-      </div>
+      </div> */}
     </div>
   );
 };
