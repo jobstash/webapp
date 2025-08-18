56b6efbbebe6028d8c6ff4bf091c660354f1d0fb
johnshift <johnshift.dev@gmail.com>
2025-08-18 15:46:54 +0800

    feat: add get user credentials data fetch

--- lib/auth/core/schemas.ts
+++ lib/auth/core/schemas.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod';
 
-import { optionalDataResponseSchema } from '@/lib/shared/core/schemas';
+import { optionalDataResponseSchema, permissionsSchema } from '@/lib/shared/core/schemas';
 
 // TODO: Implement actual user schema
 export const userSchema = z.object({
@@ -10,3 +10,15 @@ export type UserSchema = z.infer<typeof userSchema>;
 
 export const getUserResponseSchema = optionalDataResponseSchema(userSchema);
 export type GetUserResponseSchema = z.infer<typeof getUserResponseSchema>;
+
+export const userCredentialsSchema = z.object({
+  token: z.string(),
+  permissions: permissionsSchema,
+});
+export type UserCredentialsSchema = z.infer<typeof userCredentialsSchema>;
+
+export const getUserCredentialsResponseSchema =
+  optionalDataResponseSchema(userCredentialsSchema);
+export type GetUserCredentialsResponseSchema = z.infer<
+  typeof getUserCredentialsResponseSchema
+>;
