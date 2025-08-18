27887528cd7940e84c998e212120d038c908e315
johnshift <johnshift.dev@gmail.com>
2025-08-18 17:37:55 +0800

    refactor: destroy session in logout

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -1,7 +1,17 @@
+import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server';
 
-// TODO: Implement actual logout functionality
+import { getIronSession } from 'iron-session';
+
+import { SESSION_OPTIONS } from '@/lib/shared/core/constants';
+import { SessionSchema } from '@/lib/auth/core/schemas';
+
 export const POST = async () => {
+  const reqCookies = await cookies();
+  const session = await getIronSession<SessionSchema>(reqCookies, SESSION_OPTIONS);
+
+  session.destroy();
+
   return NextResponse.json({
     success: true,
     message: 'User logged out successfully',
