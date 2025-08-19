c3d6d4047027809f76130bf489d6837a3b12f36f
johnshift <johnshift.dev@gmail.com>
2025-08-19 15:04:04 +0800

    refactor: require authenticated in profile page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,4 +1,18 @@
+'use client';
+
+import { notFound } from 'next/navigation';
+
+import { useAuthSelector } from '@/lib/auth/providers';
+
 const ProfilePage = () => {
+  const hasPermission = useAuthSelector((snapshot) => {
+    return snapshot.matches('authenticated');
+  });
+
+  if (!hasPermission) {
+    return notFound();
+  }
+
   return <div>ProfilePage</div>;
 };
 export default ProfilePage;
