c0a0bc315b70e88b1ace37157b59d896e489a56c
johnshift <johnshift.dev@gmail.com>
2025-08-19 08:45:32 +0800

    refactor: explicit logout-session naming

---
+++ lib/auth/data/logout-session.ts
@@ -0,0 +1,24 @@
+import { genericResponseSchema } from '@/lib/shared/core/schemas';
+
+import { kyFetch } from '@/lib/shared/data/ky-fetch';
+
+export const logoutSession = async () => {
+  const response = await kyFetch.post('/api/auth/logout');
+  const jsonData = await response.json();
+
+  const { success: parseSuccess, data: parsedResponse } =
+    genericResponseSchema.safeParse(jsonData);
+
+  if (!parseSuccess) {
+    // TODO: add logs, sentry
+    throw new Error('Invalid logout response');
+  }
+
+  const { success, message } = parsedResponse;
+  if (!success) {
+    // TODO: add logs, sentry
+    throw new Error(message);
+  }
+
+  return message;
+};
