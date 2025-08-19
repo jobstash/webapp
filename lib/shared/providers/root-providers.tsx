3ac960c4b1c21cd612de09a1f530d03a39c146da
johnshift <johnshift.dev@gmail.com>
2025-08-19 13:49:44 +0800

    feat: add lazy provider to root provider

--- lib/shared/providers/root-providers.tsx
+++ lib/shared/providers/root-providers.tsx
@@ -5,11 +5,15 @@ import { NuqsAdapter } from 'nuqs/adapters/next/app';
 import { ReactQueryProvider } from './react-query-provider';
 import { ThemeProvider } from './theme-provider';
 
+import { LazyAuthProvider } from '@/lib/auth/providers';
+
 export const RootProviders = ({ children }: React.PropsWithChildren) => {
   return (
     <ThemeProvider attribute='class' defaultTheme='dark' disableTransitionOnChange>
       <NuqsAdapter>
-        <ReactQueryProvider>{children}</ReactQueryProvider>
+        <ReactQueryProvider>
+          <LazyAuthProvider>{children}</LazyAuthProvider>
+        </ReactQueryProvider>
       </NuqsAdapter>
     </ThemeProvider>
   );
