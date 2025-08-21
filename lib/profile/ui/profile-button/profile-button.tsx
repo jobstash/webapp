b567b8b01ba10b23549555d496573e2fd39754a4
johnshift <johnshift.dev@gmail.com>
2025-08-21 15:22:09 +0800

    feat: convert profile button into a dropdown menu

--- lib/profile/ui/profile-button/profile-button.tsx
+++ lib/profile/ui/profile-button/profile-button.tsx
@@ -1,8 +1,20 @@
 'use client';
 
+import Link from 'next/link';
+
+import { LogOutIcon, UserIcon } from 'lucide-react';
+
 import { LOADING_LOGOUT_STATES } from '@/lib/auth/core/constants';
 
+import { Avatar, AvatarFallback, AvatarImage } from '@/lib/shared/ui/base/avatar';
 import { Button } from '@/lib/shared/ui/base/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/lib/shared/ui/base/dropdown-menu';
 import { Spinner } from '@/lib/shared/ui/spinner';
 
 import { useAuthActorRef, useAuthSelector } from '@/lib/auth/providers';
@@ -18,14 +30,38 @@ export const ProfileButton = () => {
   };
 
   return (
-    <Button
-      size='lg'
-      variant='secondary'
-      className='h-10 w-32'
-      disabled={isLoading}
-      onClick={handleLogout}
-    >
-      {isLoading ? <Spinner className='size-4' /> : 'Logout'}
-    </Button>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          size='lg'
+          variant='secondary'
+          className='flex h-10 w-40 items-center justify-start gap-0.5 px-2'
+          disabled={isLoading}
+        >
+          {isLoading ? (
+            <Spinner className='size-6' />
+          ) : (
+            <Avatar className='size-7'>
+              <AvatarImage src='https://github.com/shadcn.pngx' />
+              <AvatarFallback className='bg-neutral-700'>CN</AvatarFallback>
+            </Avatar>
+          )}
+          <span className='min-w-0 flex-1 truncate'>duckdegen.eth</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align='end'>
+        <DropdownMenuItem asChild>
+          <Link href='/profile'>
+            <UserIcon className='size-4.5' />
+            Profile
+          </Link>
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem className='text-destructive' onClick={handleLogout}>
+          <LogOutIcon className='size-4 text-destructive' />
+          Logout
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 };
