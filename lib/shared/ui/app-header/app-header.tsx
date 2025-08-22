3aade9ca3a370de5f49c0df5662a6e65411c9d6d
johnshift <johnshift.dev@gmail.com>
2025-08-22 14:19:17 +0800

    style: adjust header padding and spacing

--- lib/shared/ui/app-header/app-header.tsx
+++ lib/shared/ui/app-header/app-header.tsx
@@ -9,11 +9,11 @@ interface Props {
 export const AppHeader = ({ userAction }: Props) => {
   return (
     <div className='sticky top-0 z-40 flex justify-center bg-background/40 backdrop-blur-lg'>
-      <div className='flex h-16 w-full max-w-7xl items-center px-4 lg:h-20'>
-        <div className='w-72'>
+      <div className='flex h-16 w-full max-w-7xl items-center gap-3 px-4 lg:h-20 lg:gap-6'>
+        <div className='w-fit lg:w-72'>
           <Brand />
         </div>
-        <div className='flex grow items-center'>
+        <div className='flex grow items-center lg:px-6'>
           <SearchHeader />
         </div>
         <div className='grid place-items-center'>{userAction}</div>
