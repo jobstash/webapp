24c984c8c901a4a860274790b0b878084513400d
johnshift <johnshift.dev@gmail.com>
2026-02-07 14:53:47 +0800

    refactor: remove jobs section from profile
    Jobs tab is not relevant to profile management. Remove the
    route, components, and hooks for job matching/pillar links.

--- src/app/(main)/profile/accounts/page.tsx
+++
@@ -1,5 +0,0 @@
-import { ProfileAccounts } from '@/features/profile/components/profile-accounts';
-
-const AccountsPage = () => <ProfileAccounts />;
-
-export default AccountsPage;
--- src/app/(main)/profile/jobs/page.tsx
+++
@@ -1,5 +0,0 @@
-import { ProfileJobs } from '@/features/profile/components/profile-jobs';
-
-const JobsPage = () => <ProfileJobs />;
-
-export default JobsPage;
