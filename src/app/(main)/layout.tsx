9aad03971898e91f75fc0e12562dd0e9bae88202
johnshift <johnshift.dev@gmail.com>
2026-02-08 10:39:38 +0800

    fix: ensure page content fills viewport height to push footer below fold

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -6,7 +6,9 @@ type Props = Readonly<React.PropsWithChildren>;
 const MainLayout = ({ children }: Props) => (
   <>
     <AppHeader />
-    <main className='mx-auto max-w-7xl px-2 pt-4'>{children}</main>
+    <main className='mx-auto min-h-[calc(100vh-4rem)] max-w-7xl px-2 pt-4 lg:min-h-[calc(100vh-5rem)]'>
+      {children}
+    </main>
     <AppFooter />
   </>
 );
